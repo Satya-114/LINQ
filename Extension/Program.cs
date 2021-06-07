40c333ec39547c7b5670f3a4c0c5e61cf5c43ed7
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-07 16:26:11 +0530

    Add files via upload

---
+++ Extension/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            IEnumerable<int> EvenNumbers = Enumerable.Where(intList, n => n % 2 == 0);
+            foreach(var item in EvenNumbers)
+            
+                {
+                Console.WriteLine(item);
+                } 
+
+            Console.ReadKey();
+        }
+    }
+}

ef81d9f74dd1533d2d6fbbf5cf8a18d80df69b74
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-07 09:03:43 +0530

    Add files via upload

---
+++ Method Syntax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                3, 4, 5, 6, 7, 20 ,50
+            };
+            //LINQ Query using Method Syntax
+            var MethodSyntax = integerList.Where(obj => obj > 5).ToList();
+           
+            foreach (var item in MethodSyntax)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

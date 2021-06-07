2dd4ac0a5640a33a96f34c299591a2d28046ad9f
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-07 09:06:07 +0530

    Add files via upload

---
+++ Mixed Syntax/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Mixed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+            //LINQ Query using Mixed Syntax
+            var MethodSyntax = (from obj in integerList
+                                where obj > 5
+                                select obj).Sum();
+            
+            Console.Write("Sum Is : " + MethodSyntax);
+
+            Console.ReadKey();
+        }
+    }
+}
